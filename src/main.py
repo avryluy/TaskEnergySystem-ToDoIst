fb8415210c698ec86f669bdf8fd16c4f7c346d0e
Avry Luy <avryluy@mailbox.org>
2026-06-14 20:39:57 -0500

    start building main func

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
 # import visualizations
-# import todist_api
-# import data_handler
+from utils import credentials as c
+from utils import todoist_api, data_handler
+
+
 
 
 
 
 def main():
-    
+    data = {}
+    data['archive'] = c.FILE_PATH
+    api = todoist_api.ToDoistAPIClient()
+    data["projects"] = api.get_projects()
+    data["api_tasks"] = api.get_tasks_by_date
+    handler = data_handler.DataProcessor(data)
     return 0
