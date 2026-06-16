1e633b98b07cedc338b4e7a717f0feb3dbf9f532
Avry Luy <avryluy@mailbox.org>
2026-06-15 19:35:02 -0500

    add data handler to main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-# import visualizations
+# import visualizations as v
 from utils import credentials as c
 from utils import todoist_api, data_handler
 
@@ -14,4 +14,7 @@ def main():
     data["projects"] = api.get_projects()
     data["api_tasks"] = api.get_tasks_by_date
     handler = data_handler.DataProcessor(data)
+    handler.combine_tasks()
+    handler.merge_dataframes()
+    handler.save_to_csv()
     return 0
